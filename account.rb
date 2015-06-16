a2f57b9f6fb6870b68feaf39a0cbbbb6789cf399
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-06-15 22:40:47 -0700

    Withdraw works

--- account.rb
+++ account.rb
@@ -1,9 +1,5 @@
 # Create an Account class with a minimum of 6 specs. The class should have the following methods:
 
-# self.new(id, initial_balance): creates a new instance with the instance variable id and 'initial_balance' assigned
-# Account cannot be created with initialize negative balance - this will raise an ArgumentError (Google this)
-# #withdraw(amount): The input amount gets taken out of the account as result of an ATM transaction. Return value should be the updated account balance.
-# Does not allow the account to go negative - Will output a warning message and return the original un-modified balance
 # #deposit(amount): Adds the input amount to the account balance as a result of an ATM transaction. Return value should be the updated account balance
 # #balance: Returns the current account balance
 # Create a SavingsAccount class with a minimum of 6 specs. The class should inherit behavior from the Account class. It should include updated logic within the following methods:
@@ -20,13 +16,21 @@
 # Example: If the interest rate is 0.25% and the balance is $10,000, then the interest is $25 and the new balance becomes $10,025.
 #
 class Account
-  attr_accessor :id, :initial_balance
+  attr_accessor :id, :initial_balance, :balance
+
+  class NegativeBalanceError < StandardError; end
 
   def initialize(id, initial_balance)
     check_balance initial_balance
 
     @id = id
     @initial_balance = initial_balance
+    @balance = initial_balance
+  end
+
+  def withdraw amount
+    check_withdrawal amount
+    @balance -= amount
   end
 
   private
@@ -34,5 +38,10 @@ class Account
   def check_balance balance
     raise ArgumentError if balance < 0
   end
-end
 
+  def check_withdrawal amount
+    if amount > self.balance
+      raise NegativeBalanceError, "You only can withdraw $#{balance}!"
+    end
+  end
+end
--- account_spec.rb
+++ account_spec.rb
@@ -13,4 +13,19 @@ describe Account do
       expect{ Account.new(1, -10) }.to raise_error ArgumentError
     end
   end
+
+  describe '.withdraw' do
+    let(:account) { Account.new(1, 100) }
+    it "subtracts amount from balance" do
+      account.withdraw 10
+      expect(account.balance).to eq 90
+      account.withdraw 15
+      expect(account.balance).to eq 75
+    end
+
+    it "does not allow balance to go negative" do
+      expect { account.withdraw 110 }.to raise_error Account::NegativeBalanceError, "You only can withdraw $100!"
+      expect(account.balance).to eq 100
+    end
+  end
 end
