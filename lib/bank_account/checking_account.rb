f8a165956e42a9d6311b18f5daeed79ed6c8555b
Hsing-Hui Hsu <hsing-hui@carezone.com>
2015-06-16 17:37:49 -0700

    withdraw_using_checks and reset works

--- lib/bank_account/checking_account.rb
+++ lib/bank_account/checking_account.rb
@@ -1,10 +1,62 @@
-# #withdraw_using_check(amount): The input amount gets taken out of the account as a result of a check withdrawal. Returns the updated account balance.
-#
-# Allows the account to go into overdraft up to -$10 but not any lower The
-# user is allowed three free check uses in one month, but any subsequent use
-# adds a $2 transaction fee
-# #reset_checks: Resets the number of checks used to zero
-
 class CheckingAccount < Account
+
+  attr_accessor :checks_used, :month
+
   TRANSACTION_FEE = 1
+  MONTHLY_FREE_CHECKS = 3
+  OVERDRAFT_MAX = 10
+
+  class OverdraftError < StandardError; end
+
+  def initialize id, initial_balance
+    @checks_used = 0
+    @month = current_month
+    super
+  end
+
+  def reset_checks
+    @checks_used = 0
+  end
+
+  def withdraw_using_check amount
+    check_for_overdraft amount
+    calculate_fee
+    self.balance -= amount
+  end
+
+  private
+
+  def calculate_fee
+    used_checks_count = checks_used_this_month
+    if used_checks_count >= MONTHLY_FREE_CHECKS
+      check_withdrawal_fee
+    end
+
+    self.checks_used += 1
+  end
+
+  def check_for_overdraft amount
+    max_withdrawal = self.balance + OVERDRAFT_MAX
+    if amount > max_withdrawal
+      raise OverdraftError, "You only can withdraw $#{max_withdrawal}!"
+    end
+  end
+
+  def checks_used_this_month
+    if current_month != @month
+      @month = current_month
+      reset_checks
+    else
+      @checks_used
+    end
+  end
+
+  def check_withdrawal_fee
+    self.balance -= 2
+  end
+
+  def current_month
+    Time.now.month
+  end
+
 end
--- spec/lib/checking_account_spec.rb
+++ spec/lib/checking_account_spec.rb
@@ -1,3 +1,5 @@
+require 'timecop'
+
 describe CheckingAccount do
   describe '#initialize' do
     let(:account) { CheckingAccount.new(1, 10) }
@@ -14,6 +16,13 @@ describe CheckingAccount do
       account = CheckingAccount.new 1, 0
       expect(account.initial_balance).to eq 0
     end
+
+    it "sets current month" do
+      now = Time.new(2015, 5, 1)
+      Timecop.freeze(now)
+      account = CheckingAccount.new 1, 100
+      expect(account.month).to eq 5
+    end
   end
 
   describe '#withdraw' do
@@ -28,6 +37,73 @@ describe CheckingAccount do
     it "does not allow balance to go negative" do
       expect { account.withdraw 110 }.to raise_error Account::NegativeBalanceError, "You only can withdraw $100!"
       expect(account.balance).to eq 100
+    end
+  end
+
+  describe '#withdraw_using_check' do
+    let(:account) { CheckingAccount.new(1, 100) }
+    it "deducts amount from balance" do
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 90
+    end
+
+    it "allows overdraft of up to -$10" do
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 90
+      account.withdraw_using_check 100
+      expect(account.balance).to eq(-10)
+    end
+
+    it "does not allow overdraft of over -$10" do
+      expect { account.withdraw_using_check 120 }.to raise_error CheckingAccount::OverdraftError, "You only can withdraw $110!"
+      expect(account.balance).to eq 100
+    end
+
+    it "allows three free check withdrawals per month" do
+      start_of_month = Time.new(2015, 3, 1)
+      within_month_1 = Time.new(2015, 3, 2)
+      within_month_2 = Time.new(2015, 3, 3)
+      within_month_3 = Time.new(2015, 3, 4)
+      within_month_4 = Time.new(2015, 3, 5)
+      next_month_1 = Time.new(2015, 4, 1)
+      next_month_2 = Time.new(2015, 4, 2)
+
+      Timecop.freeze(start_of_month)
+
+      Timecop.travel(within_month_1)
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 90
+
+      Timecop.travel(within_month_2)
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 80
+
+      Timecop.travel(within_month_3)
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 70
+
+      Timecop.travel(within_month_4)
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 58
+
+      Timecop.travel(next_month_1)
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 48
+
+      Timecop.travel(next_month_2)
+      account.withdraw_using_check 10
+      expect(account.balance).to eq 38
+    end
+  end
+
+  describe "#reset_checks" do
+    let(:account) { CheckingAccount.new(1, 10) }
+    it "sets number of checks used to 0" do
+      expect(account.checks_used).to eq 0
+      account.withdraw_using_check 10
+      expect(account.checks_used).to eq 1
+      account.reset_checks
+      expect(account.checks_used).to eq 0
     end
   end
 end
